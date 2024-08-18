0501b6e6d2563103ecbdd98d2800e330a49a9aa2
shivang-16 <shivangyadav121@gmail.com>
2024-08-19 03:27:58 +0530

    added scraping

---
+++ websites.py
@@ -0,0 +1,9 @@
+websites = {
+    "youtube": "https://www.youtube.com",
+    "linkedin": "https://www.linkedin.com",
+    "twitter": "https://www.twitter.com",
+    "facebook": "https://www.facebook.com",
+    "instagram": "https://www.instagram.com",
+    "github": "https://www.github.com",
+    # Add more websites here
+}
